43532389dc90046f18f5ca1dfd6c0fa67f2c6bd0
mateoU1297 <castro.mateo@correounivalle.edu.co>
2023-07-11 12:16:27 -0500

    se crea repositorio

---
+++ src/main/java/com/springboot/app/msusuarioscommons/SpringbootMsUsuariosCommonsApplication.java
@@ -0,0 +1,8 @@
+package com.springboot.app.msusuarioscommons;
+
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootMsUsuariosCommonsApplication {
+
+}
---
+++ src/test/java/com/springboot/app/msusuarioscommons/SpringbootMsUsuariosCommonsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.springboot.app.msusuarioscommons;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringbootMsUsuariosCommonsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
